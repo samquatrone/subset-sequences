2c70903b5da93dee3bc9aab13f533cc329a996d3
Sam Quatrone <samquatrone@gmail.com>
2024-04-26 16:56:10 -0600

    found new sequences, reformatted to .csv (couldn't push 7-3)

--- sequence_verifier.py
+++ sequence_verifier.py
@@ -4,13 +4,26 @@ from itertools import permutations
 
 def get_normal_form(s: str):
     seen_chars = set()
-    current_id = 1
-    normal_form = [char for char in s]
+    current_id = 0
+    # normal_form = [char for char in s]
+
+    map = {
+            0:"a", 
+            1:"b", 
+            2:"c", 
+            3:"d", 
+            4:"e",
+            5:"f",
+            6:"g",
+            7:"h",
+            8:"i",
+            9:"j",           
+        }
     
     for char in str(s):
         if char not in seen_chars:
             seen_chars.add(char)
-            s = s.replace(char, str(current_id))
+            s = s.replace(char, map[current_id])
             current_id += 1
     
     return s
@@ -19,12 +32,10 @@ def count_unique_strings(strings):
     normal_forms = set()
     
     for s in strings:
-        normal_form = get_normal_form(s)
-        print(f'{s} -> {normal_form}')
-        if normal_form not in normal_forms:
-            
-            normal_forms.add(normal_form)
-        
+        # normal_form = get_normal_form(s)
+        # print(f'{s} -> {normal_form}')
+        if s not in normal_forms:
+            normal_forms.add(s)
         
     
     unique_count = len(normal_forms)
@@ -42,15 +53,15 @@ def read_strings_from_file(filename):
 
 
 
-def verify_sequence(sequence, alphabet, word_length):
-    expected_length = comb(len(alphabet), word_length) + word_length-1
+def verify_sequence(sequence, alphabet_len, word_length):
+    expected_length = comb(alphabet_len, word_length) + word_length-1
     if len(sequence) != expected_length:
         print(f'FAILURE: Sequence {sequence} is not the expected length.')
         return False
     
     word_set = set()
     
-    for i in range(comb(len(alphabet), word_length)):
+    for i in range(comb(alphabet_len, word_length)):
         word = tuple(sequence[i:i+word_length])
         if len(word) != len(set(word)):
             print(f'FAILURE: Sequence \'{sequence}\' has repeated characters in word: \'{word}\'.')
@@ -62,27 +73,35 @@ def verify_sequence(sequence, alphabet, word_length):
         
         word_set.add(tuple(sorted(word)))
 
-    print(f'SUCCESS: Sequence \'{sequence}\'.')
+    # print(f'SUCCESS: Sequence \'{sequence}\'.')
 
     return True
 
-def sanitize_sequences(sequences, alphabet, word_length):
-    return [s for s in sequences if verify_sequence(s, alphabet, word_length)]
+def sanitize_sequences(sequences, alphabet_len, word_length):
+    return [s for s in sequences if verify_sequence(s, alphabet_len, word_length)]
 
 
 
 if __name__ == '__main__':
-    alphabet = ('a','b','c','d','e')
+    alphabet_len = 7
     word_length = 2
     # verify_sequence('abcgfedgfbegcafecdgafdbgaedbceabfcdab', alphabet, word_length)
 
     # filename = 's7-2.txt'
     # unique_count = count_unique_strings(filename)
     # print(f'Number of unique strings: {unique_count}')
-    filename = 'strings.txt'  # Replace with your file name
-    strings = sanitize_sequences(read_strings_from_file(filename), alphabet, word_length)
 
     # strings = ['abca', 'dbcd', 'abcd']
+
+    # test = "0123452413026405163560"
+
+    # print(get_normal_form(test))
+
+
+    filename = 'strings.txt'  # Replace with your file name
+    strings = [s.replace(" ", "") for s in read_strings_from_file(filename)]
+    strings = sanitize_sequences(strings, alphabet_len, word_length)
+
     unique_count = count_unique_strings(strings)
     print(f'Number of unique strings: {unique_count}')
 
